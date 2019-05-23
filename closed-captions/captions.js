ff7aedac8997e2528eb8976bb8da21b1cf639f5b
KayleeRoxanne <33269494+KayleeRoxanne@users.noreply.github.com>
2019-05-23 15:55:49 +0200

    Defenitieve Versie
    Open in Firefox

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -1,109 +1,50 @@
-captions[0] = ['3.8','8.1'];
-captions[1] = ['8.1','10'];
-captions[2] = ['10.8','12.5'];
-	//And blood-black nothingness began to spin
-captions[3] = ['12.5','15.6'];
-captions[4] = ['17.369','20'];
-captions[5] = ['20.1','23.3'];
-	//Fuck off, skin-job!
-captions[6] = ['23.3','25.2'];
-	
-captions[7] = ['26.8','31.6'];
-captions[8] = ['32.7','33'];
-captions[9] = ['33.2','34'];
-	//Have you ever been in an institution?
-captions[10] = ['34','36'];
-captions[11] = ['36','36.3'];
-captions[12] = ['36.5','37.3'];
-	//Do they keep you in a cell?
-captions[13] = ['37.3','38.6'];
-captions[14] = ['38.6','38.9'];
-captions[15] = ['39.1','39.9'];
-	//When you're not performing your duties, do they keep you in a little box?
-captions[16] = ['39.9','42.8'];
-captions[17] = ['42.8','43.1'];
-captions[18] = ['43.3','44.1'];
-	//Interlinked
-captions[19] = ['44','44.5'];
-captions[20] = ['45','45.7'];
-	//What's it like to hold the hand of someone you love?
-captions[21] = ['45.7','47.6'];
-captions[22] = ['47.6','48.1'];
-captions[23] = ['48.3','49'];
-	//Did they teach you how to feel, finger to finger?
-captions[24] = ['49.1','50.9'];
-captions[25] = ['50.9','51.4'];
-captions[26] = ['51.6','52.3'];
-	//Do you long for having your heart interlinked?
-captions[27] = ['52.3','54'];
-captions[28] = ['54','54.5'];
-captions[29] = ['54.7','55.4'];
-	//Do you dream about being interlinked?
-captions[30] = ['55.4','57.2'];
-captions[31] = ['57.2','58'];
-	//What's it like to hold your child in your arms?
-captions[32] = ['58','59.8'];
-captions[33] = ['59.8','60.3'];
-captions[34] = ['60.3','61'];
-	//Do you feel that there's a part of you that's missing?
-captions[35] = ['61','62.7'];
-captions[36] = ['62.7','63.2'];
-captions[37] = ['63.2','63.9'];
-	//Within cells interlinked.
-captions[38] = ['63.9','65.2'];
-captions[39] = ['65.2','66.6'];
-	//Why don't you say that three times: "within cells interlinked".
-captions[40] = ['66.6','69.1'];
-
-captions[41] = ['69.1','70.6'];
-captions[42] = ['70.6','72.2'];
-captions[43] = ['72.2','74.2'];
-	// We're done.
-captions[44] = ['77.1','78.7'];
-captions[45] = ['80','81.738'];
-captions[46] = ['82.2','84.3'];
-captions[47] = ['85.4','87'];
-
-//end manual labour
-captions[48] = ['87.68','88.18'];
-captions[49] = ['88.38','88.88'];
-// Have you ever been in an institution. Cells
-captions[50] = ['88.88','91.24'];
-captions[51] = ['91.34','91.84'];
-captions[52] = ['91.98','95.52'];
-// -Cells
-captions[53] = ['95.52','96.02'];
-captions[54] = ['96.12','100.2'];
-captions[55] = ['100.3','102.4'];
-// -Interlinked
-captions[56] = ['102.64','103.3'];
-captions[57] = ['103.3','104.38'];
-captions[58] = ['104.54','105.42'];
-// Dreadfully
-captions[59] = ['105.42','105.92'];
-captions[60] = ['106','106.5'];
-captions[61] = ['106.64','109.28'];
-// -Dreadfully
-captions[62] = ['109.28','109.8'];
-captions[63] = ['109.8','112.4'];
-captions[64] = ['112.4','113'];
-// Dreadfully distinct
-captions[65] = ['113','114.06'];
-captions[66] = ['114.12','114.98'];
-captions[67] = ['115.02','115.56'];
-// -Dark
-captions[68] = ['115.56','116.26'];
-captions[69] = ['116.26','117.4'];
-captions[70] = ['117.4','118.44'];
-// Withing one stem
-captions[71] = ['118.44','120'];
-captions[72] = ['120.46','121.28'];
-captions[73] = ['121.4','122.66'];
-// -And dreadfully distinct
-captions[74] = ['122.66','123.86'];
-captions[75] = ['123.98','124.92'];
-captions[76] = ['124.92','125.52'];
-// A tall white fountain played
-captions[77] = ['125.52','127.28'];
-captions[78] = ['128.18','130.14'];
-captions[79] = ['132.58','135'];
+// Do you think people love...
+captions[0] = ['4.00', '6.67'];
+captions[1] = ['6.67', '7.64'];
+captions[2] = ['8.05', '11.00'];
+// Honey you never need a bigger size
+captions[3] = ['11.00', '12.93'];
+captions[4] = ['12.93', '16.91'];
+captions[5] = ['16.91', '19.62'];
+// Squeeze those cheeks, sister.
+captions[6] = ['19.62', '21.33'];
+captions[7] = ['21.98', '24.71'];
+captions[8] = ['25.00', '26.15'];
+// Suck it in.
+captions[9] = ['26.15', '29.19'];
+captions[10] = ['29.19', '32.25'];
+captions[11] = ['32.25', '33.25'];
+// Oh I can't breath
+captions[12] = ['33.85', '37.36'];
+captions[13] = ['38.00', '40.44'];
+captions[14] = ['40.44', '42.95'];
+// aaaauuhh...
+captions[15] = ['43.00', '44.95'];
+captions[16] = ['45.25', '46.33'];
+captions[17] = ['48.33', '50.54'];
+// oke, you need a pair of hot shoes
+captions[18] = ['50.95', '53.33'];
+captions[19] = ['54.25', '56.93'];
+captions[20] = ['56.93', '58.53'];
+// 14.
+captions[21] = ['59.00', '60.22'];
+captions[22] = ['61.22', '66.25'];
+captions[23] = ['66.25', '67.58'];
+// A ten. 
+captions[24] = ['67.94', '68.42'];
+captions[25] = ['68.42', '72.25'];
+captions[26] = ['72.95', '75.25'];
+// Let's see.
+captions[27] = ['75.54', '76.95'];
+captions[28] = ['78.91', '81'];
+captions[29] = ['81.01', '82.94'];
+// Hm, I don't know. 
+captions[30] = ['84.86', '88.14'];
+captions[31] = ['88.8', '90.06'];
+captions[32] = ['95.81', '96.25'];
+// around.
+captions[33] = ['98.93', '99.93'];
+captions[34] = ['101.93', '103.54'];
+captions[35] = ['114.43', '116.43'];
+// You know what. Maybe a nice dress.
+captions[36] = ['116.43', '117.9'];
--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,27 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0.10, //0.logo bounce *
+	0.90, //1.logo bounce *
+    2, //2.logo bounce *
+    3.9, // 3. gordijn *
+    4.5, //4.logo bounce *
+	17.5, //5.shaking *
+    19, //6. p> waaahh *
+	20, //7.ademIn *
+	31, //8.rits *
+	42.8, //9.onderbroek *
+	44.5, //10.gil color transition *
+	65, //11.schreeuwen
+	67, //12. WHAT color transition *
+	71, //13.slaan op de kont *
+	77, //14. Gordijn
+    92, //15. stand up straight *
+    104, //16. diep ademhalen
+    106, //17. knoop knapt los *
+    106.5, //18. glas breekt *
+    108, //19. hakken breken *
+    110, //20. kleding scheurt *
+    112, //21. geluid spannend *
+    115 // 22. spannend geluid stopt *
 ];
